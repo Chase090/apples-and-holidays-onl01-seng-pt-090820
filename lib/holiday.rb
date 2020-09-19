1269bb23324e09aa26bdaff2dc43971976db1f7a
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-19 10:40:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday_name|
+  puts "#{season.capitalize}"
+  
 end
 
 def all_holidays_with_bbq(holiday_hash)
