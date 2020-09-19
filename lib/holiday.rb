96ce6e8e2eee7d0dc53ccc533b47ed9d33e5cd1e
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-19 10:15:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,8 +40,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # modifies the original hash by adding supplies of a new holiday to a season
- holiday_hash[season][holiday_name] = supply_array
-  holiday_hash
+
 end
 
 def all_winter_holiday_supplies(holiday_hash)
